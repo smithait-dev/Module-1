7ca8dbba5e64ba660bd5348e61f66cd12e024567
smithait-dev <smitha.it@gmail.com>
2026-03-07 14:05:10 +0000

    initial commit

---
+++ Lesson 1 Activity 1/main.py
@@ -0,0 +1,6 @@
+#printing a greeting message
+print("Hello codingle")
+print("My name is Pranav")
+print("I am in class 4K")
+#performing and printing a simple calculation
+print("The result of 3+5 is:",3+5)
