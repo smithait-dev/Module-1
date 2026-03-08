2b3d0d0538f77885c6753c47ed68fb5c81ede993
smithait-dev <smitha.it@gmail.com>
2026-03-08 13:22:02 +0000

    initial commit2

---
+++ Lesson 2 Activity 1/first.py
@@ -0,0 +1,7 @@
+#Print a sentence
+print("Welcome to world of programming")
+print(7)
+print("My name is teletran 1\n")
+print("My school name is Codingle","I am in class",9)
+print("Welcome to ",end="")
+print("Python")
