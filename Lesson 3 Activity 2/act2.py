ab774f2273507a899bf926cbec302df8a0d94189
smithait-dev <smitha.it@gmail.com>
2026-03-14 13:38:56 +0000

    Typecasting

---
+++ Lesson 3 Activity 2/act2.py
@@ -0,0 +1,21 @@
+name="Jai"
+age=12
+marks=78.21
+is_pass=True
+string="93.42"
+print("The data type of name is",type(name))
+print("The data type of age is",type(age))
+print("The data type of marks is", type(marks))
+print("The data type of pass is", type(is_pass))
+age=str(age)
+print("The data type of age is", type(age))
+print("Jai is",age)
+marks=int(marks)
+print("The data type of marks is", type(marks))
+print("Jai got",marks,"on his exam.")
+string=float(string)
+print("The data type of string is",type(string))
+print("String =",string)
+name=float(name)
+print("The data type of name is", type(name))
+print("His name is",name)
