097fea83a3e53cf0d3d4de51c16c5c78bcb13ca1
smithait-dev <smitha.it@gmail.com>
2026-03-15 13:16:04 +0000

    Avg

---
+++ Lesson 4 Activity 1/L4act1.py
@@ -0,0 +1,9 @@
+tree1=98
+tree2=94
+tree3=41
+tree4=95
+tree5=11
+sum=tree1+tree2+tree3+tree4+tree5
+print("The sum of the height of the 5 trees in inches are",sum)
+avg=sum/5
+print("The average height of the 5 trees in inches is",avg)
