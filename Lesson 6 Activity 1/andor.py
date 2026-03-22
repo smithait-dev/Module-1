d9e7a60f60cf53199ea72cef7ed8eb7af031d9ac
smithait-dev <smitha.it@gmail.com>
2026-03-22 13:24:33 +0000

    logicaloperators

---
+++ Lesson 6 Activity 1/andor.py
@@ -0,0 +1,18 @@
+num=4
+num2=8
+num3=12
+if num and num2 and num3:
+    print("All the numbers have a boolean value as True")
+else:
+    print("At least one number has a boolean value as false")
+a=0
+b=2
+c=-8
+if a>0 or b>0 or c>0:
+    print("Either of the numbers are greater than zero")
+else:
+    print("No number is greater than zero")
+if b>0 and c>0:
+    print("Either of the numbers are greater than zero")
+else:
+    print("No number is greater than zero")
