975d4341109b73ec2148fbd73578dd81da94af19
smithait-dev <smitha.it@gmail.com>
2026-03-28 13:39:36 +0000

    lef/right shift

---
+++ Lesson 7 Activity 2/bitwiseoperator.py
@@ -0,0 +1,16 @@
+a=10
+b=-10
+print("a >> 1=",a>>1)
+print("a >> 1=",b>>1)
+x=5
+y=-5
+print("x << 1=",x<<1)
+print("y << 1=",y<<1)
+q=20
+w=-20
+print("q >>2",q>>2)
+print("w >>2",w>>2)
+e=40
+r=-40
+print("e<<2",e<<2)
+print("r<<2",r<<2)
