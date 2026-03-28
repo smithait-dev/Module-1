a64a4246650ddda755fc4a9f51af1470dc3bfb35
smithait-dev <smitha.it@gmail.com>
2026-03-28 12:35:14 +0000

    conditionaltemps

---
+++ Summer Times/conditionalstatement.py
@@ -0,0 +1,7 @@
+temp=int(input("Enter the temperature outside:"))
+if temp >=20:
+    print("It is warm enough to wear light clothes.")
+elif 15 <= temp < 20:
+    print("You can wear light clothes but you might feel a bit cool")
+else:
+    print("It is too cold to wear light clothes.")
