b4011c78c6c9916e095ed344ba6796bd43c93321
smithait-dev <smitha.it@gmail.com>
2026-03-28 13:20:10 +0000

    is or is not

---
+++ Lesson 7 Activity 1/isoperator.py
@@ -0,0 +1,25 @@
+x=7
+if (type(x) is int):
+    print("True")
+else:
+    print("False")
+
+y=2.17
+if (type(y)is not float):
+    print("False")
+else:
+    print("True")
+w=2026
+z=2026
+if x==y:
+    print("They are equal")
+else:
+    print("False")
+a=2
+b=2
+if a is b:
+    print("A and B are the same")
+c=3
+if a is not c:
+    print("A and C do not have the same indentity")
+
