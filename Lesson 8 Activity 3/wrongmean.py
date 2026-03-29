b95e517410881ac7eac6daf8f40badae0fe0a7a8
smithait-dev <smitha.it@gmail.com>
2026-03-29 14:32:04 +0100

    correctmean

---
+++ Lesson 8 Activity 3/wrongmean.py
@@ -0,0 +1,10 @@
+mean=38
+total_num=40
+wrongnum=36
+correctnum=56
+sum=(total_num*mean)
+print("The wrong sum is:",sum)
+total=sum+(correctnum-wrongnum)
+print("The correct total is:",total)
+acc_mean=total/total_num
+print("The correct mean is:",acc_mean)
