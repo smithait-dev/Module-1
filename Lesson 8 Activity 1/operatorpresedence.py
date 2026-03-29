fbaf4353874f1e5f077e80c9c0acc567650e3498
smithait-dev <smitha.it@gmail.com>
2026-03-29 14:16:39 +0100

    BODMAS

---
+++ Lesson 8 Activity 1/operatorpresedence.py
@@ -0,0 +1,14 @@
+a=23
+b=9
+c=43
+d=5
+result=(a+b)*(c/d)
+result2=(a+b)*c/d
+print(result)
+print(result2)
+name="Pranav"
+age=16
+if name=="Pranav" or name=="Jack" and age <= 18:
+    print("Hello I am",name)
+else:
+    print("I don't know you")
